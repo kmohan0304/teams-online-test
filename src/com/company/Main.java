2efd9a0925e90d56f6e0bc72116bc713b3bab091
Ivan Petrychenko <ivan.petrychenko@tact.ai>
2021-10-01 14:54:16 -0700

    improvements of language, added numbers to tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -66,17 +66,22 @@ public class Main {
 
         Item.printList( apple );
 
-        //Task 1. Understanding helper methods
-        //do the same, but not using the Item constructor, preferably in one line of code
+        // Task 1. Understanding helper methods
+        // Please, create the same list, but now do not use the Item constructor.
+        // Preferably do it in one line of code
 
-        //Task 2.
-        // print values of all items of the list using `while` loop . You can use `for` loop if you want
+        // Task 2. Navigating the list
+        // Please, print all items of the list form Task 1 using `while` loop.
+        // You can use `for` loop if you want.
 
-        //print all items without `while` loop (you may want to add another method to Item class )
+        // Task 3. More elegant navigating
+        // Please, print all items without using any loop.
+        // You may want to add another method to Item class.
 
 
-        //change Item to be able to build a tree instead of a list.
-        // Create this tree:
+        // Task 4. More complex data structure.
+        // Please, change the Item class to be able to build a tree instead of a list.
+        // Using it, please create the following tree:
         //                root
         //             /        \
         //            /          \
@@ -84,22 +89,32 @@ public class Main {
         //      /  |  \         /  |    \
         //  cat, dog, horse  apple orange banana
 
-        //go over the tree and print all items, depth first
+        // Task 5. Navigating the tree
+        // Please, implement a separate static method that goes
+        // over the tree and print all items, depth first
 
-        //same but now breadth first
+        // Task 6. Navigating the tree, another strategy
+        // Please, implement another method that does the same, but now breadth first.
 
-        //using same Item class, define a graph of air flights :
-        //Los Angeles <-> San Francisco
-        //Los Angeles <-> New York
-        //New York <-> Boulder
+        // Task 7. Visitor
+        // Please, modify the method from Task 6 to be able to apply any code to every item in the
+        // tree, without changing the method.
+        // To tes, write a code that only prints item values if they are longer than 3 characters.
 
-        // write a code that can find a route between two cities
-        // find a way from LA to boulder
+        // Task 7. Graph data structure
+        // Here is the information about cities connected by direct flights:
+        // Los Angeles <-> San Francisco
+        // Los Angeles <-> New York
+        // New York <-> Boulder
+        // Before you start coding, please, anser this question:
+        // "Do you need any changes in Item class code to be able to store this information?"
+        // Please, create a data structure using Item class to capture this information.
 
-        // add length of each flight in hours
+        // Task 8. Navigating the graph.
+        // Please, write a separate method that can find a route between two given cities using data
+        // structure from Task 7.
+        // Using this method, find a way from LA to Boulder.
 
-        // write calculate the total length of flight for all connections between two cities
-        // test for Boulder -> New York
 
 
     }
